fcc1d352980478207fd9e04a0d399d827cb073a1
Teekam saini <teekamsingodiyasaini@gmail.com>
2026-01-06 19:28:11 +0530

    add jk_flip_flop.v

---
+++ rtl/flip_flops/jk_ff.v
@@ -0,0 +1,22 @@
+module jk_ff(
+    input  wire clk,j,reset,k,
+    output reg q
+);
+
+always @(posedge clk or posedge reset) begin
+
+    if (reset) begin
+        q<=1'b0;
+    end
+    else begin 
+        case ({j,k})
+        2'b00: q<= q;
+        2'b01: q<= 1'b0;
+        2'b10: q<= 1'b1;
+        2'b11: q<= ~q;
+        endcase
+    end
+end
+
+
+endmodule
